2c71ab16035992871d3daa41ad463879f2159dfc
Georgia <georgia@webantic.co.uk>
2018-09-17 20:54:56 +0100

    ship can leave port

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -3,11 +3,24 @@ const Ship = require('../src/ship');
 let ship = null;
 
 beforeEach(() => {
- ship = new Ship('My Big Boat');
+  ship = new Ship('My Big Boat');
+});
+
+describe('Name Ship', () => {
+  it('gives the ship a name', () => {
+    expect(ship.name).toEqual('My Big Boat');
+  });
 });
 
 describe('Ship has starting point', () => {
- it('gives the ship a starting point', () => {
-  expect(ship.startingPort).toEqual('Liverpool');
- });
+  it('gives the ship a starting point', () => {
+    expect(ship.startingPort).toEqual('Liverpool');
+  });
+});
+
+describe('Ship should be able to set sail from a port', () => {
+  it('can leave the current port', () => {
+    ship.setSail();
+    expect(ship.sail).toBeTruthy();
+  });
 });
--- src/ship.js
+++ src/ship.js
@@ -1,6 +1,24 @@
 function Ship(name) {
- this.name = name;
- this.startingPort = 'Liverpool';
+  this.name = name;
+  this.startingPort = 'Liverpool';
+  this.sail = true;
 }
+
+Ship.prototype = {
+  get canSail() {
+    if (this.sail === true) {
+      return true;
+    }
+    return false;
+  },
+};
+
+Ship.prototype.setSail = function setSail() {
+  if (!this.canSail) {
+    throw new Error('Your ship cannot leave port!');
+  } else {
+    this.sail = true;
+  }
+};
 
 module.exports = Ship;
